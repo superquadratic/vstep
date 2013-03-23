bfc663418f2160bafc5485a2b8e1fd9901e65d87
Guido Lorenz <code@superquadratic.net>
2013-03-23 15:44:33 +0100

    Add copy of AGain as vStep plugin stub

---
+++ src/vstep.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "public.sdk/source/vst2.x/audioeffectx.h"
+
+//-------------------------------------------------------------------------------------------------------
+class VStep : public AudioEffectX
+{
+public:
+	VStep(audioMasterCallback audioMaster);
+	~VStep();
+
+	// Processing
+	virtual void processReplacing(float** inputs, float** outputs, VstInt32 sampleFrames);
+	virtual void processDoubleReplacing(double** inputs, double** outputs, VstInt32 sampleFrames);
+
+	// Program
+	virtual void setProgramName(char* name);
+	virtual void getProgramName(char* name);
+
+	// Parameters
+	virtual void setParameter(VstInt32 index, float value);
+	virtual float getParameter(VstInt32 index);
+	virtual void getParameterLabel(VstInt32 index, char* label);
+	virtual void getParameterDisplay(VstInt32 index, char* text);
+	virtual void getParameterName(VstInt32 index, char* text);
+
+	virtual bool getEffectName(char* name);
+	virtual bool getVendorString(char* text);
+	virtual bool getProductString(char* text);
+	virtual VstInt32 getVendorVersion();
+
+protected:
+	float fGain;
+	char programName[kVstMaxProgNameLen + 1];
+};
