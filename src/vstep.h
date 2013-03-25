41dbf0f1bdf548f7f7d44ff8c9c173f4f025a169
Guido Lorenz <code@superquadratic.net>
2013-03-25 22:16:46 +0100

    Remove gain param, use memcpy

--- src/vstep.h
+++ src/vstep.h
@@ -17,19 +17,12 @@ public:
 	virtual void setProgramName(char* name);
 	virtual void getProgramName(char* name);
 
-	// Parameters
-	virtual void setParameter(VstInt32 index, float value);
-	virtual float getParameter(VstInt32 index);
-	virtual void getParameterLabel(VstInt32 index, char* label);
-	virtual void getParameterDisplay(VstInt32 index, char* text);
-	virtual void getParameterName(VstInt32 index, char* text);
-
+	// Info
 	virtual bool getEffectName(char* name);
 	virtual bool getVendorString(char* text);
 	virtual bool getProductString(char* text);
 	virtual VstInt32 getVendorVersion();
 
 protected:
-	float fGain;
 	char programName[kVstMaxProgNameLen + 1];
 };
